d805fe0006514a24de5c827ccfc7d789a5872a41
SyntaxEvgen <syntax-evgen@yandex.ru>
2022-04-23 18:02:11 +0300

    Добавьте файлы проекта.

---
+++ WebApiGeekBrains/Controllers/TemperatureSpecifiedTimeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiGeekBrains.Controllers
+{
+
+    /// <summary>
+    /// 
+    //Возможность сохранить температуру в указанное время
+    //Возможность отредактировать показатель температуры в указанное время
+    //Возможность удалить показатель температуры в указанный промежуток времени
+    //Возможность прочитать список показателей температуры за указанный промежуток времени
+
+    /// </summary>
+    public class TemperatureSpecifiedTimeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
